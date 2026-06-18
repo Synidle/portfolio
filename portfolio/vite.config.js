629666dfcc3db7951862ec6b382db8fc9df95c00
Synidle <73420926+Synidle@users.noreply.github.com>
2026-06-18 16:59:25 +0100

    try this

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,7 +1,7 @@
 import {defineConfig} from "vite";
 
 export default defineConfig({
-    base: "https://synidle.github.io/portfolio/",
+    base: "/portfolio/",
     build: {
         outDir: "docs",
         emptyOutDir: true,
