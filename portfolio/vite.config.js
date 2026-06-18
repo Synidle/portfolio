e1adf0452a62fe88a6d74b4780470c8be60e830a
Synidle <73420926+Synidle@users.noreply.github.com>
2026-06-18 16:46:29 +0100

    perhaps fixed

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,7 +3,7 @@ import {defineConfig} from "vite";
 export default defineConfig({
     base: "https://synidle.github.io/portfolio/",
     build: {
-        outDir: "../docs",
+        outDir: "docs",
         emptyOutDir: true,
     }
 });
