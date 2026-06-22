34107d5a02baebecf92e3562b756925eb731f455
Synidle <73420926+Synidle@users.noreply.github.com>
2026-06-22 17:20:48 +0100

    ProjectButton object

--- Source/JS/projectList.js
+++ Source/JS/projectList.js
@@ -1,11 +1,27 @@
 let projectListElement = document.querySelector("#project-list");
 
+/**
+ * @param {Project} project 
+ */
+function ProjectButton(project) {
+    this.project = project;
+
+    this.button = document.createElement("button");
+    this.button.classList.add("project-button");
+
+    this.image = document.createElement("img");
+    this.image.src = `Source/ProjectData/${project.images[0].source}`;
+    this.image.alt = project.images[0].alt;
+
+    this.name = document.createElement("label");
+    this.name.classList.add("project-name");
+    this.name.innerHTML = project.name;
+
+    this.button.appendChild(this.image);
+    this.button.appendChild(this.name);
+}
+
 for (let p of projects) {
-    let projectButton = document.createElement("button");
-    projectButton.classList.add("project-button");
-    projectButton.innerHTML = `
-        <img src="Source/ProjectData/${p.images[0].source}" alt="${p.images[0].alt}"> <br>
-        <label class="project-name">${p.name}</label>
-    `;
-    projectListElement.appendChild(projectButton);
+    let projectButton = new ProjectButton(p);
+    projectListElement.appendChild(projectButton.button);
 }
