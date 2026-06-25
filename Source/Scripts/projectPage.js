7f3f808b347cadadcc4a2ea8c19629d88f7aeeef
Synidle <73420926+Synidle@users.noreply.github.com>
2026-06-25 14:19:10 +0100

    fill project information on page

--- Source/Scripts/projectPage.js
+++ Source/Scripts/projectPage.js
@@ -2,3 +2,23 @@ const projectID = sessionStorage.getItem(KEY_SELECTED_PROJECT);
 const project = projectsLookup.get(projectID); 
 
 document.querySelector("title").innerHTML = `Ben Lockyer | ${project.name}`;
+document.getElementById("project-title").innerHTML = project.name;
+document.getElementById("project-role").innerHTML = project.role;
+document.getElementById("project-description").innerHTML = project.description;
+document.getElementById("project-link").innerHTML = project.link;
+document.getElementById("project-year").innerHTML = project.year;
+document.getElementById("project-status").innerHTML = project.status;
+document.getElementById("project-format").innerHTML = project.format;
+document.getElementById("project-tags").innerHTML = getListItemsFrom(project.tags);
+document.getElementById("project-skills").innerHTML = getListItemsFrom(project.skills);
+
+/**
+ * 
+ * @param {any[]} array 
+ */
+function getListItemsFrom(array) {
+    let val = "";
+    for (let i of array)
+        val += `<li>${i}</li>`;
+    return val; 
+}
