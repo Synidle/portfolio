6a50c81591bff1881910bd99e32415f89c535e01
Synidle <73420926+Synidle@users.noreply.github.com>
2026-06-25 21:17:00 +0100

    display inspirations on page

--- Source/Scripts/projectPage.js
+++ Source/Scripts/projectPage.js
@@ -13,14 +13,26 @@ document.getElementById("project-status").innerHTML = project.status;
 document.getElementById("project-format").innerHTML = project.format;
 document.getElementById("project-tags").innerHTML = getListItemsFrom(project.tags);
 document.getElementById("project-skills").innerHTML = getListItemsFrom(project.skills);
+document.getElementById("project-inspirations").innerHTML = getListItemsFrom(project.inspirations, formatInspiration);
 
 /**
  * 
  * @param {any[]} array 
+ * @param {function} formatter
  */
-function getListItemsFrom(array) {
+function getListItemsFrom(array, formatter=null) {
     let val = "";
-    for (let i of array)
+    for (let i of array) {
+        if (formatter != null) i = formatter(i); 
         val += `<li>${i}</li>`;
+    }
     return val; 
+}
+
+/**
+ * 
+ * @param {Inspiration} inspiration 
+ */
+function formatInspiration(inspiration) {
+    return `<i>${inspiration.name}</i> (${inspiration.author}, ${inspiration.year}): ${inspiration.description}.`
 }
