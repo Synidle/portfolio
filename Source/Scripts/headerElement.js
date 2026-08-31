20687b9cf312d3c2caa251c24277bf962011c2a1
Synidle <73420926+Synidle@users.noreply.github.com>
2026-08-31 11:40:05 +0100

    navigation links

--- Source/Scripts/headerElement.js
+++ Source/Scripts/headerElement.js
@@ -1,32 +1,24 @@
 const headerElement = document.getElementById("header");
 
-if (headerElement != null) {
-    const navElement = document.createElement("nav");
-    const homeButton = document.createElement("button");
-    const projectsButton = document.createElement("button");
-
-    navElement.id = "main-nav";
-    homeButton.id = "nav-home";
-    projectsButton.id = "nav-projects";
-
-    homeButton.innerHTML = "Home";
-    projectsButton.innerHTML = "Projects";
-
-    linkButtonToPage(homeButton, "homepage.html");
-    linkButtonToPage(projectsButton, "project-list.html");
-
-    headerElement.appendChild(navElement);
-    navElement.appendChild(homeButton);
-    navElement.appendChild(projectsButton); 
+if (!headerElement) {
+    throw new Error("Header element not found.");
 }
 
-/**
- * 
- * @param {HTMLButtonElement} button 
- * @param {string} page 
- */
-function linkButtonToPage(button, page) {
-    button.addEventListener("click", function() {
-        open(page, "_self");
-    });
+const navElement = document.createElement("nav");
+navElement.id = "main-nav";
+
+/** id, text, page */
+const pages = [
+    ["nav-home", "Home", "homepage.html"],
+    ["nav-projects", "Projects", "project-list.html"]
+];
+
+for (const [id, text, page] of pages) {
+    const link = document.createElement("a");
+    link.id = id;
+    link.href = page;
+    link.textContent = text;
+    navElement.appendChild(link); 
 }
+
+headerElement.appendChild(navElement);
