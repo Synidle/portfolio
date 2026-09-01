617308ee796cad1defd0c11677800eefa0b2841e
Synidle <73420926+Synidle@users.noreply.github.com>
2026-09-01 21:08:09 +0100

    dropdown

--- Source/Scripts/filterElement.js
+++ Source/Scripts/filterElement.js
@@ -7,17 +7,23 @@ function createFilterElement(parentElement, parameters) {
     for (const parameter of parameters) {
         const fieldset = document.createElement("fieldset");
         const legend = document.createElement("legend");
+        const details = document.createElement("details");
+        const summary = document.createElement("summary");
         const label = document.createElement("label");
         const input = document.createElement("input");
 
         const ps = parameter.toString(); 
 
         legend.innerHTML = ps; 
+        summary.innerHTML = ps; 
         label.innerHTML = `Any ${ps}`;
         input.type = "checkbox"; input.name = ps; input.value = "";
         
         label.appendChild(input); 
-        fieldset.appendChild(label); 
+        details.appendChild(summary); 
+        details.appendChild(label); 
+        fieldset.appendChild(legend);
+        fieldset.appendChild(details);
 
         for (const [key, value] of Object.entries(parameter)) {
             const label = document.createElement("label");
@@ -25,7 +31,7 @@ function createFilterElement(parentElement, parameters) {
             label.innerHTML = value;
             input.type = "checkbox"; input.name = value; input.value = value;
             label.appendChild(input);
-            fieldset.appendChild(label);
+            details.appendChild(label);
         }
 
         parentElement.appendChild(fieldset);
